05e0ae9e27ffac918e583ea4e59ac96dcbec004a
Hermes Francisco <franciscohermes2000@hotmail.com>
2020-06-15 19:20:09 -0300

    database index.js - 15/06/2020

---
+++ src/app/models/Usuario.js
@@ -0,0 +1,18 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Usuario extends Model {
+    static init(sequelize){
+        super.init(
+            {
+                nome : Sequelize.STRING,
+                email : Sequelize.STRING,
+                password_hash : Sequelize.STRING,
+            },
+            {
+                sequelize,
+            }
+        );
+    }
+}
+
+export default Usuario;
